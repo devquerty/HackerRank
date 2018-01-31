7fa1a41e45345f3cdae97341bc3e0cd5531b9a63
Gaurav <devquerty@gmail.com>
2018-01-31 11:26:05 +0530

    implemented the First method for Array Rotation using TDD

--- CrackingtheCodingInterviewChallenges/CrackingtheCodingInterviewChallenges.Tests/ArrayLeftRotationTests.cs
+++ CrackingtheCodingInterviewChallenges/CrackingtheCodingInterviewChallenges.Tests/ArrayLeftRotationTests.cs
@@ -6,9 +6,128 @@ namespace CrackingtheCodingInterviewChallenges.Tests
     [TestClass]
     public class ArrayLeftRotationTests
     {
+        //For any given array of size X perform N left rotations where N<=X
+
         [TestMethod]
-        public void TestMethod1()
+        [ExpectedException(typeof(ArgumentOutOfRangeException), "Rotations Greater the Array size should not be allowed.")]
+        public void RotationsGreaterThenArraySizeShouldThrowArgumentOutOfRangeException()
         {
+            var rotations = 7;
+            var array = new int[5] { 1, 2, 3, 4, 5 };
+
+            var arrayRotation = new ArrayLeftRotation();
+
+            var rotatedArray = arrayRotation.RotateLeft(rotations, array);
+
+            CollectionAssert.AreEqual(array, rotatedArray);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), "Negative Rotations should not be allowed.")]
+        public void NegativeRotationsShouldThrowArgumentOutOfRangeException()
+        {
+            var rotations = 7;
+            var array = new int[5] { 1, 2, 3, 4, 5 };
+
+            var arrayRotation = new ArrayLeftRotation();
+
+            var rotatedArray = arrayRotation.RotateLeft(rotations, array);
+
+            CollectionAssert.AreEqual(array, rotatedArray);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), "Null Input Array should not be allowed.")]
+        public void NullArrayShouldThrowArgumentNullException()
+        {
+            var rotations = 7;
+
+            int[] array = null;
+
+            var arrayRotation = new ArrayLeftRotation();
+
+            var rotatedArray = arrayRotation.RotateLeft(rotations, array);
+
+            CollectionAssert.AreEqual(array, rotatedArray);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Empty Input Array should not be allowed.")]
+        public void EmptyArrayShouldThrowArgumentException()
+        {
+            var rotations = 7;
+
+            var array = new int[0];
+
+            var arrayRotation = new ArrayLeftRotation();
+
+            var rotatedArray = arrayRotation.RotateLeft(rotations, array);
+
+            CollectionAssert.AreEqual(array, rotatedArray);
+        }
+
+        [TestMethod]
+        public void ZeroRotationsShouldReturnSameArray()
+        {
+            var rotations = 0;
+
+            var inputArray = new int[5] { 1, 2, 3, 4, 5 };
+
+            var expectedArray = new int[5] { 1, 2, 3, 4, 5 };
+
+            var arrayRotation = new ArrayLeftRotation();
+
+            var rotatedArray = arrayRotation.RotateLeft(rotations, inputArray);
+
+            CollectionAssert.AreEqual(expectedArray, rotatedArray);
+        }
+
+        [TestMethod]
+        public void OneRotationsShouldReturnUpdatedArray()
+        {
+            var rotations = 1;
+
+            var inputArray = new int[5] { 1, 2, 3, 4, 5 };
+
+            var expectedArray = new int[5] { 2, 3, 4, 5, 1 };
+
+            var arrayRotation = new ArrayLeftRotation();
+
+            var rotatedArray = arrayRotation.RotateLeft(rotations, inputArray);
+
+            CollectionAssert.AreEqual(expectedArray, rotatedArray);
+        }
+
+        [TestMethod]
+        public void FourRotationsShouldReturnUpdatedArray()
+        {
+            var rotations = 4;
+
+            var inputArray = new int[5] { 1, 2, 3, 4, 5 };
+
+            var expectedArray = new int[5] { 5, 1, 2, 3, 4 };
+
+            var arrayRotation = new ArrayLeftRotation();
+
+            var rotatedArray = arrayRotation.RotateLeft(rotations, inputArray);
+
+            CollectionAssert.AreEqual(expectedArray, rotatedArray);
+        }
+
+        [TestMethod]
+        public void RotationsEqualToArrayLengthShouldReturnSameArray()
+        {
+            var inputArray = new int[5] { 1, 2, 3, 4, 5 };
+
+            var rotations = inputArray.Length;
+
+            var expectedArray = new int[5] { 1, 2, 3, 4, 5 };
+
+            var arrayRotation = new ArrayLeftRotation();
+
+            var rotatedArray = arrayRotation.RotateLeft(rotations, inputArray);
+
+            CollectionAssert.AreEqual(expectedArray, rotatedArray);
         }
     }
 }
--- CrackingtheCodingInterviewChallenges/CrackingtheCodingInterviewChallenges/ArrayLeftRotation.cs
+++ CrackingtheCodingInterviewChallenges/CrackingtheCodingInterviewChallenges/ArrayLeftRotation.cs
@@ -8,5 +8,35 @@ namespace CrackingtheCodingInterviewChallenges
 {
     public class ArrayLeftRotation
     {
+        public int[] RotateLeft(int rotations, int[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException("array", "Array should be a non null with at least one member in it.");
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException("array", "Array should have at least one member in it.");
+            }
+
+            if (rotations < 0 || rotations > array.Length)
+            {
+                throw new ArgumentOutOfRangeException("rotations", "Rotations should be a > 0 and < array.Length.");
+            }
+
+            int[] tempArray = new int[rotations];
+
+            Array.Copy(array, tempArray, rotations);
+
+            for (int i = 0; i < array.Length - rotations; i++)
+            {
+                array[i] = array[i + rotations];
+            }
+
+            Array.Copy(tempArray, 0, array, array.Length - rotations, rotations);
+
+            return array;
+        }
     }
 }
