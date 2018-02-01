97dffad74e26d01699207d4faa00ae7a7c47afbc
Gaurav <devquerty@gmail.com>
2018-02-01 12:18:02 +0530

    Added 2 more solutions to the array rotation problem.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fibonacci(80));
+            Console.ReadLine();
+        }
+
+        static int Fibonacci(int n)
+        {
+            if (n <= 1)
+                return 1;
+
+            return Fibonacci(n-1) +  Fibonacci(n - 2);
+        }
+    }
+}
--- CrackingtheCodingInterviewChallenges/CrackingtheCodingInterviewChallenges/ArrayLeftRotation.cs
+++ CrackingtheCodingInterviewChallenges/CrackingtheCodingInterviewChallenges/ArrayLeftRotation.cs
@@ -8,7 +8,7 @@ namespace CrackingtheCodingInterviewChallenges
 {
     public class ArrayLeftRotation
     {
-        public int[] RotateLeft(int rotations, int[] array)
+        public int[] RotateLeft1(int rotations, int[] array)
         {
             if (array == null)
             {
@@ -35,6 +35,67 @@ namespace CrackingtheCodingInterviewChallenges
             }
 
             Array.Copy(tempArray, 0, array, array.Length - rotations, rotations);
+
+            return array;
+        }
+
+        public int[] RotateLeft2(int rotations, int[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException("array", "Array should be a non null with at least one member in it.");
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException("array", "Array should have at least one member in it.");
+            }
+
+            if (rotations < 0 || rotations > array.Length)
+            {
+                throw new ArgumentOutOfRangeException("rotations", "Rotations should be a > 0 and < array.Length.");
+            }
+
+            int[] tempArray = new int[array.Length];
+
+            Array.Copy(array, rotations, tempArray, 0, array.Length - rotations);
+
+            Array.Copy(array, 0, tempArray, array.Length - rotations, rotations);
+
+            return tempArray;
+        }
+
+        public int[] RotateLeft(int rotations, int[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException("array", "Array should be a non null with at least one member in it.");
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException("array", "Array should have at least one member in it.");
+            }
+
+            if (rotations < 0 || rotations > array.Length)
+            {
+                throw new ArgumentOutOfRangeException("rotations", "Rotations should be a > 0 and < array.Length.");
+            }
+
+            var length = array.Length;
+
+            for (int i = 1; i <= rotations; i++)
+            {
+                var first = array[0];
+
+                for (int j = 0; j < length - 1; j++)
+                {
+                    array[j] = array[j + 1];
+                }
+
+                array[length - 1] = first;
+
+            }
 
             return array;
         }
