2c56f392d9ded1bbf3d3f0957a6452b999b47ea5
Gaurav <devquerty@gmail.com>
2018-02-02 11:55:05 +0530

    Added Solution to Anagram Problem

---
+++ CrackingtheCodingInterviewChallenges/CrackingtheCodingInterviewChallenges.Tests/AnagramFinderTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingtheCodingInterviewChallenges.Tests
+{
+    [TestClass]
+    public class AnagramFinderTests
+    {
+
+        [TestMethod]
+        public void SameStringsAreAnagramWithZeroDeletions()
+        {
+            var str1 = "abc";
+            var str2 = "abc";
+
+            var expectedResult = 0;
+
+            var anagramFinder = new AnagramFinder();
+
+            var actualResult = anagramFinder.FindDeletions(str1, str2);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        [TestMethod]
+        public void DiffStringsAreAnagramWithThreeDeletions()
+        {
+            var str1 = "abcxyz";
+            var str2 = "abc";
+
+            var expectedResult = 3;
+
+            var anagramFinder = new AnagramFinder();
+
+            var actualResult = anagramFinder.FindDeletions(str1, str2);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        [TestMethod]
+        public void DiffStringsAreAnagramWithSixDeletions()
+        {
+            var str1 = "abcxyz";
+            var str2 = "aabcbb";
+
+            var expectedResult = 6;
+
+            var anagramFinder = new AnagramFinder();
+
+            var actualResult = anagramFinder.FindDeletions(str1, str2);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ CrackingtheCodingInterviewChallenges/CrackingtheCodingInterviewChallenges/AnagramFinder.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingtheCodingInterviewChallenges
+{
+    public class AnagramFinder
+    {
+        public int FindDeletions(string str1, string str2)
+        {
+            var countArray = new int[26];
+            var asciiofA = (int)'a';
+
+            var deletionCount = 0;
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                var asciival = (int)str1[i];
+                ++countArray[asciival - asciiofA];
+            }
+
+            for (int i = 0; i < str2.Length; i++)
+            {
+                var asciival = (int)str2[i];
+                --countArray[asciival - asciiofA];
+            }
+
+            for (int i = 0; i < countArray.Length; i++)
+            {
+                deletionCount += Math.Abs(countArray[i]);
+            }
+
+            return deletionCount;
+        }
+        public int FindDeletions1(string str1, string str2)
+        {
+            var str1Array = str1.ToCharArray();
+
+            var str2Array = str2.ToCharArray();
+
+            var deletionCount = 0;
+
+            for (int i = 0; i < str1Array.Length; i++)
+            {
+                for (int j = 0; j < str2Array.Length; j++)
+                {
+                    if (str1Array[i] == str2Array[j])
+                    {
+                        str1Array[i] = str2Array[j] = '0';
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < str1Array.Length; i++)
+            {
+                if (str1Array[i] != '0')
+                {
+                    deletionCount++;
+                }
+            }
+
+            for (int i = 0; i < str2Array.Length; i++)
+            {
+                if (str2Array[i] != '0')
+                {
+                    deletionCount++;
+                }
+            }
+
+            return deletionCount;
+        }
+    }
+}
